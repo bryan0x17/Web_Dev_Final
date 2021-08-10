024631e05be28f92e24a8ef1ede4590d4b0b96e4
Orobs <50989230+bryan0x17@users.noreply.github.com>
2021-08-09 18:57:17 -0600

    Finish part 1

--- finalproject/datasets/data.js
+++ finalproject/datasets/data.js
@@ -69,28 +69,12 @@ function search() {
     // First checks which dataset is being searched and calls the appropriate search function
     if (firstCriteria === 'Request ID') {
         threeOneOneSearch();
-    } else if (firstCriteria === 'Cuisine') {
-        if (firstSearch) {
-            returnedData = returnedData.filter((element) => {
-                if (element['cuisine_description']) {
-                    return element['cuisine_description'].toLowerCase().includes(firstSearch);
-                }
-            });
-        }
-        printRestoResults(returnedData);
-    } else if (firstCriteria === 'School Name') {
-        if (firstSearch) {
-            returnedData = returnedData.filter((element) => {
-                if (element['schoolname']) {
-                    return element['schoolname'].toLowerCase().includes(firstSearch);
-                }
-            });
-        }
-        printSchoolResults(returnedData);
-    }
-    // Checks if both search bars are empty, and if so clears the result table
-    if (!firstSearch && !secondSearch) {
-        clearSearch();
+    } else if (firstCriteria === 'Sector') {
+        crimeSearch();
+    } else if (firstCriteria === 'Camera ID') {
+        cameraSearch();
+    } else if (firstCriteria === 'Permit Number') {
+        landSearch();
     }
 }
 
@@ -114,25 +98,156 @@ function threeOneOneSearch() {
     if (secondSearch) {
         returnedData = returnedData.filter((element) => {
             if (element['service_name']) {
-                return element['service_name'].toLowerCase().includes(firstSearch);
+                return element['service_name'].toLowerCase().includes(secondSearch);
             }
         });
     }
     if (thirdSearch) {
         returnedData = returnedData.filter((element) => {
             if (element['agency_responsible']) {
-                return element['agency_responsible'].toLowerCase().includes(firstSearch);
+                return element['agency_responsible'].toLowerCase().includes(thirdSearch);
             }
         });
     }
     if (fourthSearch) {
         returnedData = returnedData.filter((element) => {
             if (element['comm_name']) {
-                return element['comm_name'].toLowerCase().startsWith(firstSearch);
+                return element['comm_name'].toLowerCase().startsWith(fourthSearch);
             }
         });
     }
     print311Results(returnedData);
+    // Checks if all search bars are empty, and if so clears the result table
+    if (!firstSearch && !secondSearch && !thirdSearch && !fourthSearch) {
+        clearSearch();
+    }
+}
+
+function crimeSearch() {
+    let firstSearch = document.getElementById('firstCriteriaValue').value.toLowerCase();
+    let secondSearch = document.getElementById('secondCriteriaValue').value.toLowerCase();
+    let thirdSearch = document.getElementById('thirdCriteriaValue').value.toLowerCase();
+    let fourthSearch = document.getElementById('fourthCriteriaValue').value;
+    // Store a new array of objects containing the filtered JSON data
+    let returnedData = data;
+    // Filter the data using every search criteria
+    if (firstSearch) {
+        returnedData = returnedData.filter((element) => {
+            if (element['sector']) {
+                return element['sector'].toLowerCase().startsWith(firstSearch);
+            }
+        });
+    }
+    if (secondSearch) {
+        returnedData = returnedData.filter((element) => {
+            if (element['community_name']) {
+                return element['community_name'].toLowerCase().startsWith(secondSearch);
+            }
+        });
+    }
+    if (thirdSearch) {
+        returnedData = returnedData.filter((element) => {
+            if (element['category']) {
+                return element['category'].toLowerCase().includes(thirdSearch);
+            }
+        });
+    }
+    if (fourthSearch) {
+        returnedData = returnedData.filter((element) => {
+            if (element['year']) {
+                return element['year'].startsWith(fourthSearch);
+            }
+        });
+    }
+    printCrimeResults(returnedData);
+    // Checks if all search bars are empty, and if so clears the result table
+    if (!firstSearch && !secondSearch && !thirdSearch && !fourthSearch) {
+        clearSearch();
+    }
+}
+
+function cameraSearch() {
+    let firstSearch = document.getElementById('firstCriteriaValue').value;
+    let secondSearch = document.getElementById('secondCriteriaValue').value.toLowerCase();
+    let thirdSearch = document.getElementById('thirdCriteriaValue').value.toLowerCase();
+    let fourthSearch = document.getElementById('fourthCriteriaValue').value.toLowerCase();
+    // Store a new array of objects containing the filtered JSON data
+    let returnedData = data;
+    // Filter the data using every search criteria
+    if (firstSearch) {
+        returnedData = returnedData.filter((element) => {
+            if (element['id']) {
+                return element['id'].startsWith(firstSearch);
+            }
+        });
+    }
+    if (secondSearch) {
+        returnedData = returnedData.filter((element) => {
+            if (element['description']) {
+                return element['description'].toLowerCase().includes(secondSearch);
+            }
+        });
+    }
+    if (thirdSearch) {
+        returnedData = returnedData.filter((element) => {
+            if (element['community']) {
+                return element['community'].toLowerCase().startsWith(thirdSearch);
+            }
+        });
+    }
+    if (fourthSearch) {
+        returnedData = returnedData.filter((element) => {
+            if (element['quadrant']) {
+                return element['quadrant'].toLowerCase().startsWith(fourthSearch);
+            }
+        });
+    }
+    printCameraResults(returnedData);
+    if (!firstSearch && !secondSearch && !thirdSearch && !fourthSearch) {
+        clearSearch();
+    }
+}
+
+function landSearch() {
+    let firstSearch = document.getElementById('firstCriteriaValue').value.toLowerCase();
+    let secondSearch = document.getElementById('secondCriteriaValue').value.toLowerCase();
+    let thirdSearch = document.getElementById('thirdCriteriaValue').value.toLowerCase();
+    let fourthSearch = document.getElementById('fourthCriteriaValue').value.toLowerCase();
+    // Store a new array of objects containing the filtered JSON data
+    let returnedData = data;
+    // Filter the data using every search criteria
+    if (firstSearch) {
+        returnedData = returnedData.filter((element) => {
+            if (element['permitnum']) {
+                return element['permitnum'].toLowerCase().startsWith(firstSearch);
+            }
+        });
+    }
+    if (secondSearch) {
+        returnedData = returnedData.filter((element) => {
+            if (element['description']) {
+                return element['description'].toLowerCase().includes(secondSearch);
+            }
+        });
+    }
+    if (thirdSearch) {
+        returnedData = returnedData.filter((element) => {
+            if (element['applicant']) {
+                return element['applicant'].toLowerCase().startsWith(thirdSearch);
+            }
+        });
+    }
+    if (fourthSearch) {
+        returnedData = returnedData.filter((element) => {
+            if (element['address']) {
+                return element['address'].toLowerCase().startsWith(fourthSearch);
+            }
+        });
+    }
+    printLandResults(returnedData);
+    if (!firstSearch && !secondSearch && !thirdSearch && !fourthSearch) {
+        clearSearch();
+    }
 }
 
 // Clears the result table
@@ -141,56 +256,89 @@ function clearSearch() {
     resultHeading.className = 'hidden';
 }
 
-// Prints the results for the rat inspection table
+// Prints the results for the 311 Requests table
 function print311Results(results) {
     let resultString = '';
     results.forEach((element) => {
         resultString += '<tr>';
-        resultString += `<td>${element['street_name']}</td>`;
-        resultString += `<td>${element['house_number']}</td>`;
-        resultString += `<td>${element['borough']}</td>`;
-        resultString += `<td>${element['inspection_date']}</td>`;
-        resultString += `<td>${element['result']}</td>`;
+        resultString += `<td>${element['service_request_id']}</td>`;
+        resultString += `<td>${element['requested_date']}</td>`;
+        resultString += `<td>${element['status_description']}</td>`;
+        resultString += `<td>${element['service_name']}</td>`;
+        resultString += `<td>${element['agency_responsible']}</td>`;
+        resultString += `<td>${element['comm_name']}</td>`;
+        resultString += `<td>${getLocationLink(element)}</td>`;
         resultString += '</tr>';
     });
     resultHeading.className = '';
     resultTable.innerHTML = resultString;
 }
 
-// Prints the results for the restaurant inspection table
-function printRestoResults(results) {
+// Prints the results for the Crime Statistics table
+function printCrimeResults(results) {
     let resultString = '';
     results.forEach((element) => {
         resultString += '<tr>';
-        resultString += `<td>${element['dba']}</td>`;
-        resultString += `<td>${element['boro']}</td>`;
-        resultString += `<td>${element['street']}</td>`;
-        resultString += `<td>${element['zipcode']}</td>`;
-        resultString += `<td>${element['cuisine_description']}</td>`;
-        resultString += `<td>${element['inspection_date']}</td>`;
-        resultString += `<td>${element['violation_description']}</td>`;
-        resultString += `<td>${element['critical_flag']}</td>`;
+        resultString += `<td>${element['sector']}</td>`;
+        resultString += `<td>${element['community_name']}</td>`;
+        resultString += `<td>${element['category']}</td>`;
+        resultString += `<td>${element['crime_count']}</td>`;
+        resultString += `<td>${element['date']}</td>`;
+        resultString += `<td>${getLocationLink(element)}</td>`;
         resultString += '</tr>';
     });
     resultHeading.className = '';
     resultTable.innerHTML = resultString;
 }
 
-// Prints the results for the cafeteria inspection table
-function printSchoolResults(results) {
+// Prints the results for the Camera Locations table
+function printCameraResults(results) {
     let resultString = '';
     results.forEach((element) => {
         resultString += '<tr>';
-        resultString += `<td>${element['schoolname']}</td>`;
-        resultString += `<td>${element['borough']}</td>`;
-        resultString += `<td>${element['street']}</td>`;
-        resultString += `<td>${element['zipcode']}</td>`;
-        resultString += `<td>${element['inspectiondate']}</td>`;
-        resultString += `<td>${element['violationdescription']}</td>`;
+        resultString += `<td>${element['id']}</td>`;
+        resultString += `<td>${element['description']}</td>`;
+        resultString += `<td>${element['quadrant']}</td>`;
+        resultString += `<td>${element['community']}</td>`;
+        resultString += `<td>${getLocationLink(element)}</td>`;
         resultString += '</tr>';
     });
-    resultHeading.classNameName = '';
+    resultHeading.className = '';
     resultTable.innerHTML = resultString;
+}
+
+// Prints the results for the Land Applications table
+function printLandResults(results) {
+    let resultString = '';
+    results.forEach((element) => {
+        resultString += '<tr>';
+        resultString += `<td>${element['permitnum']}</td>`;
+        resultString += `<td>${element['description']}</td>`;
+        resultString += `<td>${element['statuscurrent']}</td>`;
+        resultString += `<td>${element['applieddate']}</td>`;
+        resultString += `<td>${element['applicant']}</td>`;
+        resultString += `<td>${element['fromlud']}</td>`;
+        resultString += `<td>${element['proposedlud']}</td>`;
+        resultString += `<td>${element['address']}</td>`;
+        resultString += `<td>${getLocationLink(element)}</td>`;
+        resultString += '</tr>';
+    });
+    resultHeading.className = '';
+    resultTable.innerHTML = resultString;
+}
+
+// Takes the object and returns an anchor tag with a Google Maps link from the object's latitude and longitude properties
+function getLocationLink(element) {
+    let linkString = `<a href="https://www.google.com/maps/search/?api=1&query=`;
+    if (element['lat'] && element['long']) {
+        linkString += `${element['lat']},${element['long']}`;
+    } else if (element['latitude'] && element['longitude']) {
+        linkString += `${element['latitude']},${element['longitude']}`;
+    } else {
+        return 'Unavailable';
+    }
+    linkString += `" target="_blank">Location</a>`;
+    return linkString;
 }
 
 setMainListeners();
